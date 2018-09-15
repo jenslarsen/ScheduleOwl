5fe54844500acb6634a0d4198596a4c4d9cc34b7
Jens Larsen <dasuperman@gmail.com>
2018-09-15 15:25:50 -0700

    Add ViewPagerAdapter

--- app/src/main/java/com/jenslarsen/scheduleowl/MainActivity.java
+++ app/src/main/java/com/jenslarsen/scheduleowl/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jenslarsen.scheduleowl;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
     }
 }
