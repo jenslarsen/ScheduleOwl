17f47d875fe4c16c378353ac826b377f68f84998
jlarsen <G!THhe4dph0ne>
2018-09-22 06:46:10 -0700

    Add Add Terms button

--- app/src/main/java/com/jenslarsen/scheduleowl/fragmentTerms.java
+++ app/src/main/java/com/jenslarsen/scheduleowl/fragmentTerms.java
@@ -2,11 +2,14 @@ package com.jenslarsen.scheduleowl;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.jenslarsen.scheduleowl.db.Datasource;
 import com.jenslarsen.scheduleowl.model.Term;
@@ -20,12 +23,23 @@ public class fragmentTerms extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_terms, container, false);
 
-        ListView listView = rootView.findViewById(R.id.listViewTerms);
+        final ListView listView = rootView.findViewById(R.id.listViewTerms);
 
         ArrayAdapter<Term> adapter = new ArrayAdapter<>(getContext(), R.layout.listitem_tab,
                 R.id.textViewListItem, Datasource.terms);
         listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Term", "Item " + position);
+            }
+        });
+
         return rootView;
+    }
+
+    public void addTermClicked(View view) {
+        Toast.makeText(getContext(), "Add Term clicked!", Toast.LENGTH_SHORT).show();
     }
 }
