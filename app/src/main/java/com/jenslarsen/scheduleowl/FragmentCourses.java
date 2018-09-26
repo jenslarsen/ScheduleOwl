57f3b82578774ea42c060a050c2ce8701d3345c7
jlarsen <G!THhe4dph0ne>
2018-09-25 21:08:38 -0600

    Fix broken button

--- app/src/main/java/com/jenslarsen/scheduleowl/FragmentCourses.java
+++ app/src/main/java/com/jenslarsen/scheduleowl/FragmentCourses.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.jenslarsen.scheduleowl.db.Datasource;
 import com.jenslarsen.scheduleowl.model.Course;
@@ -26,6 +28,18 @@ public class FragmentCourses extends Fragment {
                 R.id.textViewListItem, Datasource.courses);
         listView.setAdapter(adapter);
 
+        Button buttonAddCourse = rootView.findViewById(R.id.buttonAddCourse);
+        buttonAddCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonAddCourseClicked();
+            }
+        });
+
         return rootView;
+    }
+
+    public void buttonAddCourseClicked() {
+        Toast.makeText(getContext(), "Add Course here", Toast.LENGTH_SHORT).show();
     }
 }
