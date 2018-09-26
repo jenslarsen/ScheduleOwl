054c979862f57da56417518249a0a211e7d40ec7
jlarsen <G!THhe4dph0ne>
2018-09-25 21:09:26 -0600

    Fix broken button

--- app/src/main/java/com/jenslarsen/scheduleowl/FragmentMentors.java
+++ app/src/main/java/com/jenslarsen/scheduleowl/FragmentMentors.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.jenslarsen.scheduleowl.db.Datasource;
 import com.jenslarsen.scheduleowl.model.Mentor;
@@ -26,6 +28,18 @@ public class FragmentMentors extends Fragment {
                 R.id.textViewListItem, Datasource.mentors);
         listView.setAdapter(adapter);
 
+        Button buttonAddMentor = rootView.findViewById(R.id.buttonAddMentor);
+        buttonAddMentor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonAddMentorClicked();
+            }
+        });
+
         return rootView;
+    }
+
+    public void buttonAddMentorClicked(){
+        Toast.makeText(getContext(), "Add Mentor clicked", Toast.LENGTH_SHORT).show();
     }
 }
