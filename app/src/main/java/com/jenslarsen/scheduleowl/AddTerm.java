bf6aebbbdcb3154b742bba8becc9751d8ce7231e
Jens Larsen <dasuperman@gmail.com>
2018-09-27 15:44:13 -0700

    Add rudimentry functionality for Save and Cancel buttons

--- app/src/main/java/com/jenslarsen/scheduleowl/AddTerm.java
+++ app/src/main/java/com/jenslarsen/scheduleowl/AddTerm.java
@@ -2,6 +2,11 @@ package com.jenslarsen.scheduleowl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.jenslarsen.scheduleowl.db.Datasource;
+import com.jenslarsen.scheduleowl.model.Term;
 
 public class AddTerm extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class AddTerm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_term);
+    }
+
+    public void buttonSaveClicked(View view) {
+        EditText editTextTitle = findViewById(R.id.editTextTitle);
+
+        String termTitle = editTextTitle.getText().toString();
+
+        Term newTerm = new Term();
+        newTerm.setTitle(termTitle);
+
+        Datasource.terms.add(newTerm);
+        finish();
+    }
+
+    public void buttonCancelClicked(View view) {
+        finish();
     }
 }
