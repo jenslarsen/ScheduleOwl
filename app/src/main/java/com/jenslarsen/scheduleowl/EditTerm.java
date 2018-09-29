467e9732d7fb6ae4634162688abc72d135e3f954
jlarsen <G!THhe4dph0ne>
2018-09-28 18:01:38 -0700

    Make Save and Cancel button work

--- app/src/main/java/com/jenslarsen/scheduleowl/EditTerm.java
+++ app/src/main/java/com/jenslarsen/scheduleowl/EditTerm.java
@@ -2,8 +2,15 @@ package com.jenslarsen.scheduleowl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.jenslarsen.scheduleowl.db.Datasource;
+import com.jenslarsen.scheduleowl.model.Term;
 
 public class EditTerm extends AppCompatActivity {
+
+    private int selectedPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +18,25 @@ public class EditTerm extends AppCompatActivity {
         setContentView(R.layout.activity_edit_term);
 
         Bundle bundle = getIntent().getExtras();
-        int position = bundle.getInt("position");
+        selectedPosition = bundle.getInt("selectedPosition");
+        Term currentTerm = Datasource.terms.get(selectedPosition);
+        EditText editTextTitle = findViewById(R.id.editTextTitle);
+        editTextTitle.setText(currentTerm.getTitle());
+    }
+
+    public void buttonSaveClicked(View view) {
+        EditText editTextTitle = findViewById(R.id.editTextTitle);
+
+        String termTitle = editTextTitle.getText().toString();
+
+        Term newTerm = new Term();
+        newTerm.setTitle(termTitle);
+
+        Datasource.terms.set(selectedPosition, newTerm);
+        finish();
+    }
+
+    public void buttonCancelClicked(View view) {
+        finish();
     }
 }
