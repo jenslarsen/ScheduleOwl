092e24da5650dd3c1cbbca2011fd3e08d2a3cf58
Jens Larsen <dasuperman@gmail.com>
2018-09-29 09:30:29 -0700

    Add Courses chooser

--- app/src/main/java/com/jenslarsen/scheduleowl/AddTerm.java
+++ app/src/main/java/com/jenslarsen/scheduleowl/AddTerm.java
@@ -1,11 +1,14 @@
 package com.jenslarsen.scheduleowl;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.jenslarsen.scheduleowl.db.Datasource;
+import com.jenslarsen.scheduleowl.model.Course;
 import com.jenslarsen.scheduleowl.model.Term;
 
 public class AddTerm extends AppCompatActivity {
@@ -14,6 +17,12 @@ public class AddTerm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_term);
+
+        final ListView listView = findViewById(R.id.listViewCourses);
+
+        ArrayAdapter<Course> adapter = new ArrayAdapter<>(this, R.layout.listitem_chooser,
+                R.id.textViewChooser, Datasource.courses);
+        listView.setAdapter(adapter);
     }
 
     public void buttonSaveClicked(View view) {
