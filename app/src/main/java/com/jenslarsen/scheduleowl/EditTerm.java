4c91a4705d9a7dbf3043721c53ba6e006af553d8
Jens Larsen <dasuperman@gmail.com>
2018-11-28 19:40:39 -0800

    Fix imports

--- app/src/main/java/com/jenslarsen/scheduleowl/EditTerm.java
+++ app/src/main/java/com/jenslarsen/scheduleowl/EditTerm.java
@@ -15,7 +15,6 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
